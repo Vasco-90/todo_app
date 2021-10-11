574c76dc704b7b18bb8a2e3a60ede9e62f7dcdce
FunkyMan4415 <funkyman@ok.de>
2021-10-11 19:35:41 +0200

    add theme to app

--- lib/common/theme/theme.dart
+++ lib/common/theme/theme.dart
@@ -3,8 +3,10 @@ import 'package:todo_app/common/theme/todo_colors.dart';
 
 final themeLight = ThemeData(
   scaffoldBackgroundColor: TodoColors.backgroundColor,
+  appBarTheme: const AppBarTheme(color: TodoColors.appBarBackgroundColor),
 );
 
 final themeDark = ThemeData(
   scaffoldBackgroundColor: TodoDarkColors.backgroundColor,
+  appBarTheme: const AppBarTheme(color: TodoDarkColors.appBarBackgroundColor),
 );
--- lib/common/theme/todo_colors.dart
+++ lib/common/theme/todo_colors.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 class TodoColors {
   static const backgroundColor = Color(0xFFF4F5F5);
+  static const appBarBackgroundColor = Color(0xFF080807);
 }
 
 class TodoDarkColors {
   static const backgroundColor = Colors.black;
+  static const appBarBackgroundColor = Color(0xFF1F1F1F);
 }
